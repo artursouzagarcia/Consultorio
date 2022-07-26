e9455d81da435e7f59ed57ea1117ec214bcfed25
tutusouza <99684708+artursouzagarcia@users.noreply.github.com>
2022-07-26 10:04:43 -0300

    migration da tabela de consulta

---
+++ Consultorio/Migrations/20220726125928_dt_consulta.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Consultorio.Migrations
+{
+    public partial class dt_consulta : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Especialidade",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true),
+                    Ativa = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Especialidade", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Paciente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true),
+                    Email = table.Column<string>(type: "TEXT", nullable: true),
+                    Celular = table.Column<string>(type: "TEXT", nullable: true),
+                    Cpf = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Paciente", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Profissional",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true),
+                    Ativo = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profissional", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EspecialidadeProfissional",
+                columns: table => new
+                {
+                    EspecialidadesId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProfissionaisId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EspecialidadeProfissional", x => new { x.EspecialidadesId, x.ProfissionaisId });
+                    table.ForeignKey(
+                        name: "FK_EspecialidadeProfissional_Especialidade_EspecialidadesId",
+                        column: x => x.EspecialidadesId,
+                        principalTable: "Especialidade",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EspecialidadeProfissional_Profissional_ProfissionaisId",
+                        column: x => x.ProfissionaisId,
+                        principalTable: "Profissional",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tb_consulta",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    data_horario = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    status = table.Column<int>(type: "INTEGER", nullable: false, defaultValue: 1),
+                    preco = table.Column<decimal>(type: "TEXT", precision: 7, scale: 2, nullable: false),
+                    PacienteId = table.Column<int>(type: "INTEGER", nullable: false),
+                    EspecialidadeId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProfissionalId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_consulta", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_tb_consulta_Especialidade_EspecialidadeId",
+                        column: x => x.EspecialidadeId,
+                        principalTable: "Especialidade",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_tb_consulta_Paciente_PacienteId",
+                        column: x => x.PacienteId,
+                        principalTable: "Paciente",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_tb_consulta_Profissional_ProfissionalId",
+                        column: x => x.ProfissionalId,
+                        principalTable: "Profissional",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EspecialidadeProfissional_ProfissionaisId",
+                table: "EspecialidadeProfissional",
+                column: "ProfissionaisId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_consulta_EspecialidadeId",
+                table: "tb_consulta",
+                column: "EspecialidadeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_consulta_PacienteId",
+                table: "tb_consulta",
+                column: "PacienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_consulta_ProfissionalId",
+                table: "tb_consulta",
+                column: "ProfissionalId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EspecialidadeProfissional");
+
+            migrationBuilder.DropTable(
+                name: "tb_consulta");
+
+            migrationBuilder.DropTable(
+                name: "Especialidade");
+
+            migrationBuilder.DropTable(
+                name: "Paciente");
+
+            migrationBuilder.DropTable(
+                name: "Profissional");
+        }
+    }
+}
